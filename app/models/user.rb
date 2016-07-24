a580deff3fff8e277cfee9b94f0e6975eb583dbf
= <=>
2016-07-24 01:07:05 +0000

    Refactor the user test suite and model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  before_save { self.email = email.downcase }
+  before_save { email.downcase! }
 
   # User validation
   validates :name, 	presence: true, length: { maximum: 50 }
