a91228e6d6326b4efd7821c9e2ee3b1162dd2e07
= <=>
2016-07-24 02:28:27 +0000

    Finish user sign up

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+	def gravatar_for(user)
+		clean_email = user.email.strip
+		lower_case = clean_email.downcase
+		hash = Digest::MD5::hexdigest(lower_case)
+		gravatar_url = "https://www.gravatar.com/avatar/#{hash}"
+		image_tag(gravatar_url, alt: user.name + "'s Profile Image", class: 'gravatar')
+	end
 end
